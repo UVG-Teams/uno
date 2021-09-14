a86d3ffd30eaf9a5389837ad55fa9173986928a0
LucaBia <gian.luca.99@hotmail.com>
2021-09-13 21:36:32 -0600

    Basic sign in

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { TextField, Button } from '@material-ui/core';
 
 import './styles.css';
 
 
 const Login = () => {
     return (
-        <div className="login_page">           
-            <div className="login_form">
-                <h1>Sign in</h1>
+        <div className='login_page'>           
+            <div className='login_form'>
+                <h1 className='login_title'>Sign in</h1>
+                <div className='inputs_login_form'>
+                    <TextField
+                        id='standard-basic' 
+                        label='Display Name'
+                        className='login_inputs'
+                    />
+                    <TextField
+                        id='standard-basic' 
+                        label='IP Adress'
+                        // color='secondary'
+                        className='login_inputs'
+                    />
+                    <TextField 
+                        id='standard-basic' 
+                        label='Room Code'
+                        className='login_inputs'
+                    />
+                </div>
+                <div className="login_button">
+                    <Button variant="contained" color="primary">Sign in</Button>
+                </div>
+
             </div>
         </div>
     )
