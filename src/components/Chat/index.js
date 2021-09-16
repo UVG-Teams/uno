0ab55c5c66a20f57515ec4a1fc1f3f8ff4f5f3fc
LucaBia <gian.luca.99@hotmail.com>
2021-09-15 19:05:19 -0600

    Chat/game board layout

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -7,7 +7,9 @@ const Chat = () => {
     return (
         <div className='chat_component'>
             <div className='chat'>
-                
+                <div className='chat_header'>
+                    <h3>Game Chat</h3>
+                </div>
             </div>
         </div>
     )
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { TextField, Button } from '@material-ui/core';
 
-import table_0 from '../Resources/Table_0.png'
+import table_0 from '../Resources/table_0.png'
 import Chat from '../Chat';
 
 import './styles.css';
 
 const Game = () => {
     return (
-        <div className='game_page' style={{ backgroundImage: `url(${table_0})`, backgroundRepeat: 'no-repeat'}}>
+        <div className='game_page'>
             <Chat />
         </div>
     )
