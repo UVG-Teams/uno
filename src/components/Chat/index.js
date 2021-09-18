d5e28e32370fd718b52a0c4cc48b4a44bf975d73
LucaBia <gian.luca.99@hotmail.com>
2021-09-17 19:23:03 -0600

    Chat UI

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { TextField, Button } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 import ReceiveBubble from './Receive_bubble';
 import SendBubble from './Send_bubble';
@@ -19,7 +21,7 @@ const Chat = () => {
                 </div>
                 <div className='chat_write_message'>
                     <input type='text' placeholder='Write your message...' className='inpMessage'></input>
-                    <button>O</button>
+                    <button className='btnMessage'><FontAwesomeIcon icon={faPaperPlane} size="2x" color="#ffffff" swapOpacity/></button>
                 </div>
             </div>
         </div>
